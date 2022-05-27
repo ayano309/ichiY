255196db1081dd932508e4d740097af40ea0113f
綾乃 <>
2022-05-27 22:43:08 +0900

    [add]Userモデル、view,controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
+
+  devise_scope :user do
+    root :to => "users/sessions#new"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    delete "logout", :to => "users/sessions#destroy"
+  end
+  
   root 'home#top'
   get 'terms', to: 'home#terms'
   get 'privacy', to: 'home#privacy'
