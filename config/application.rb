9fb67117ffbfc1846aa2a9e2872651cff50101ef
綾乃 <>
2022-05-27 22:52:47 +0900

    [add]userコントローラ

--- config/application.rb
+++ config/application.rb
@@ -13,11 +13,7 @@ module IchigoDaihuku
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
 
-    config.generators do |g|  # ここから追記
-      g.assets false          # CSS, JavaScriptファイル生成せず
-      g.skip_routes false     # trueならroutes.rb変更せず、falseなら通常通り変更
-      g.test_framework false  # testファイル生成せず
-    end
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
