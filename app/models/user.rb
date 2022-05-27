8b1d5c9660c4cd78641373968cde0501646f19e5
綾乃 <>
2022-05-27 22:59:23 +0900

    マイページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { in: 2..20 }
   validates :email, presence: true
   validates :phone, presence: true
+
+  def update_password(params, *options)
+    if params[:password].blank?
+      params.delete(:password)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
 end
