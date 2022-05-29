74b490a9fb89a6ec3e15ab7e47f98dfa1cc5333e
綾乃 <>
2022-05-29 13:14:34 +0900

    reservationモデルのvalidation追加

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -27,6 +27,17 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :capacity
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 256 }
+  validates :phone
+  validates :number_of_items, presence: true
+  validates :reservation_time, presence: true
+  validates :reservations_status, presence: true
+  validates :capacity_id, presence: true
+
+  validate :date_before_today
+  validate :start_time_not_sunday
+  validate :start_time_not_monday
 
   enum reservation_time: {
     '11:00': 0,
@@ -46,5 +57,17 @@ class Reservation < ApplicationRecord
 
   def decreased_capacity
     capacity.remaining_item - number_of_items
+  end
+
+  def date_before_today
+    errors.add(:start_time, 'は過去の日付は選択できません') if capacity.start_time < Time.zone.today
+  end
+
+  def start_time_not_saturday
+    errors.add(:start_time, 'は定休日(水曜日・土曜日)以外を選択してください') if capacity.start_time.saturday?
+  end
+
+  def start_time_not_wednesday
+    errors.add(:start_time, 'は定休日(水曜日・土曜日)以外を選択してください') if capacity.start_time.wednesday?
   end
 end
