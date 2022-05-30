9f014c1980f197219dfe9a36644b9093e0ee44a0
綾乃 <>
2022-05-30 19:09:40 +0900

    [add]管理画面TOPに新予約、新お問い合わせ一覧を表示

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,9 @@ class DashboardController < ApplicationController
   before_action :authenticate_admin!
   layout 'dashboard/dashboard'
 
-  def index; end
+  def index
+    @reservations = Reservation.all.order(created_at: 'desc')
+    @contacts = Contact.all.order(created_at: 'desc')
+  end
+  
 end
