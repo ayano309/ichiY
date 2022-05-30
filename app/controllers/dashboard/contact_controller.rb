1f79e06acd923d535cc67f90c3d7a59066200526
綾乃 <>
2022-05-30 18:13:11 +0900

    [add]管理者側のindex,

--- app/controllers/dashboard/contact_controller.rb
+++ app/controllers/dashboard/contact_controller.rb
@@ -1,7 +1,9 @@
 class Dashboard::ContactController < ApplicationController
   def index
+    @contacts = Contact.all.order(created_at: 'desc')
   end
 
   def show
+    @contact = Contact.find(params[:id])
   end
 end
