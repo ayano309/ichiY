0c49cd8ebb397cc6cba842dc7e1a9fbae5fa6660
綾乃 <>
2022-06-01 19:42:13 +0900

    [add]userモデルのRSpec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,23 +21,35 @@
 #
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  describe 'validation' do
-    it 'ユーザー名、メールアドレス、電話番号、パスワードがある場合、有効' do
-      user = build(:user)
-      expect(user).to be_valid
-    end
+RSpec.describe 'Userモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { user.valid? }
 
-    it 'ユーザー名がない場合、無効' do
-      user = build(:user, name: nil)
-      user.valid?
-      expect(user.errors[:name]).to include('を入力してください')
-    end
+    let!(:other_user) { create(:user) }
+    let(:user) { build(:user) }
 
-    it 'ユーザー名が21文字以上、無効' do
-      user = build(:user, name: 'a' * 21)
-      user.valid?
-      expect(user.errors[:name]).to include('は20文字以内で入力してください')
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        user.name = ''
+        is_expected.to eq false
+      end
+      it '2文字以上であること: 1文字は×' do
+        user.name = Faker::Lorem.characters(number: 1)
+        is_expected.to eq false
+      end
+      it '2文字以上であること: 2文字は〇' do
+        user.name = Faker::Lorem.characters(number: 2)
+        is_expected.to eq true
+      end
+      it '20文字以下であること: 20文字は〇' do
+        user.name = Faker::Lorem.characters(number: 20)
+        is_expected.to eq true
+      end
+      it '20文字以下であること: 21文字は×' do
+        user.name = Faker::Lorem.characters(number: 21)
+        is_expected.to eq false
+      end
+      
     end
 
     it 'メールアドレスがない場合、無効' do
@@ -53,5 +65,11 @@ RSpec.describe User, type: :model do
     end
   end
 
-  
+  describe 'アソシエーションのテスト' do
+    context 'reservationモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:reservations).macro).to eq :has_many
+      end
+    end
+  end
 end
