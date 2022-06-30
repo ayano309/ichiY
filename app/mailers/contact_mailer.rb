59520ff977e9acaa5e3784fe923d41199e2340e0
綾乃 <>
2022-06-30 13:57:09 +0900

    [fix]メールの宛先

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def email(contact)
     @contact = contact
     @name = contact.name.present? ? contact.name : contact.email
-    mail to: ENV['TOMAIL'], subject: '【お問い合わせ】'
-    # mail to: contact.email, subject: '【お問い合わせ】'
+    # mail to: ENV['TOMAIL'], subject: '【お問い合わせ】'
+    mail to: contact.email, subject: '【お問い合わせ】'
   end
 end
--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -3,7 +3,7 @@ class ReservationMailer < ApplicationMailer
     @reservation = reservation
     @name = reservation.name.present? ? reservation.name : reservation.email
 
-    mail to: ENV['TOMAIL'], subject: 'ご予約を受付いたしました'
-    # mail to: reservation.email, subject: 'ご予約を受付いたしました'
+    # mail to: ENV['TOMAIL'], subject: 'ご予約を受付いたしました'
+    mail to: reservation.email, subject: 'ご予約を受付いたしました'
   end
 end
