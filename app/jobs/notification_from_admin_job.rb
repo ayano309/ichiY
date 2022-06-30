aa5b214ced526c01db30886432f5a20df3c1a07c
綾乃 <>
2022-06-30 13:53:18 +0900

    [add]notification_from_admin_jobのコメント追加

--- app/jobs/notification_from_admin_job.rb
+++ app/jobs/notification_from_admin_job.rb
@@ -1,6 +1,6 @@
 class NotificationFromAdminJob < ApplicationJob
   queue_as :default
-
+  #お店からのお知らせを非同期メールにした
   def perform(msg)
     User.all.each do |user|
       NotificationFromAdminMailer.notify(user, msg).deliver_later
